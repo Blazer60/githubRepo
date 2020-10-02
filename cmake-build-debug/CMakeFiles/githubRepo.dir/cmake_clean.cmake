d0116a9373346f6c80cb05e765960afec6b4b101
Ryan Purse <ryanpurse@gmail.com>
2020-10-02 17:42:45 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/githubRepo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/githubRepo.dir/main.cpp.obj"
+  "githubRepo.exe"
+  "githubRepo.exe.manifest"
+  "githubRepo.lib"
+  "githubRepo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/githubRepo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/githubRepo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for githubRepo.
+# This may be replaced when dependencies are built.
